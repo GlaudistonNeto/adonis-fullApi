4817750aaed2d078b35990cad56bfc9dabf3f24d
GlaudistonNeto <glaudistonson@gmail.com>
2022-02-10 21:50:33 -0300

    initial commit

---
+++ database/migrations/1644538397156_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name', 50)
+      table.integer('age', 3).unsigned()
+      table.string('email', 65).unique().notNullable()
+      table.string('password', 20).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
